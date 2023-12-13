3c91f8da92883ade51fcee52091ad72910ad0884
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-12-13 16:31:30 +0530

    - Added separate methods to add prepaid and postpaid subscriber

--- lib/telephony/core/subscriber.ex
+++ lib/telephony/core/subscriber.ex
@@ -1,7 +1,15 @@
 defmodule Telephony.Core.Subscriber do
+  alias Telephony.Core.{Prepaid, Postpaid}
+
   defstruct name: nil, phone_no: nil, subscriber_type: :prepaid
 
-  def new(payload) do
+  def new(%{subscriber_type: :prepaid} = payload) do
+    payload = %{payload | subscriber_type: %Prepaid{}}
+    struct(__MODULE__, payload)
+  end
+
+  def new(%{subscriber_type: :postpaid} = payload) do
+    payload = %{payload | subscriber_type: %Postpaid{}}
     struct(__MODULE__, payload)
   end
 end
